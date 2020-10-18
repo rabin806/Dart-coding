802920e485d63afdd868e623eb1cff47bda36643
rabin806 <73080329+rabin806@users.noreply.github.com>
2020-10-18 16:47:01 -0500

    Create Calculation.java

---
+++ Calculation.java
@@ -0,0 +1,14 @@
+
+public class Calculation {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Your total score");
+		
+		Players play= new Players();
+		PlayerImpl.plays();
+
+	}
+
+}
